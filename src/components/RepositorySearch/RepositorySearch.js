3f11674efa295b56f8e8f6c1acefbadfc79373a6
Victor Nikolaev <victor@nsystem.net>
2020-06-08 22:45:17 +0300

    First commit for AvitoPro test task

---
+++ src/components/RepositorySearch/RepositorySearch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RepositorySearch extends React.Components {
+  constructor(){
+    this.state = {}
+  }
+
+  render(){
+    return(
+      <div> another haha </div>
+    )
+  }
+}
+
+export default RepositorySearch;
